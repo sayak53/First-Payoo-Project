ce24e8c8c7196ba5e56198b620b06284377fba68
sayak53 <sayakchakraborty928@gmail.com>
2024-09-20 13:26:35 +0530

    both add money and cashout feature added

--- Payoo/js/home.js
+++ Payoo/js/home.js
@@ -1,7 +1,7 @@
 document.getElementById('btn-logout').addEventListener('click',function(){
     window.location.href='index.html'
 })
-
+//  add-money function starts here
 document.getElementById('btn-addmoney').addEventListener('click',function(event){
     event.preventDefault()
     const addMoneyInput=document.getElementById('input-add-money').value
@@ -21,7 +21,9 @@ document.getElementById('btn-addmoney').addEventListener('click',function(event)
         alert("wrong pin!")
     }
 })
+//  add-money function ends here
 
+// form functions like hidden or block- starts
 document.getElementById('add-btn').addEventListener('click',function(){
     document.getElementById('add-form').setAttribute('class','block')
 })
@@ -30,3 +32,26 @@ document.getElementById('cashout-btn').addEventListener('click',function(){
     document.getElementById('cashout-form').setAttribute('class','block')
     document.getElementById('add-form').setAttribute('class','hidden')
 })
+// ends here
+ 
+// cashout function starts here
+document.getElementById('btn-outmoney').addEventListener('click',function(event){
+    event.preventDefault()
+    const cashoutMoneyInput=document.getElementById('input-cashout-money').value
+    const pinInput=document.getElementById('inputout-pin').value
+
+    if(pinInput=='1111'){
+        const accountBalance=document.getElementById('balance').innerText
+    
+        const deleteMoney=parseInt(cashoutMoneyInput)
+        const mainBalance=parseInt(accountBalance)
+
+        const newbalance=mainBalance-deleteMoney
+    
+    document.getElementById('balance').innerText=newbalance
+    }
+    else{
+        alert("wrong pin!")
+    }
+})
+// cashout function ends here
